d578a0eff4715071915f0abe21f5a8ddc4c90dd2
omer madar <wmrmdr@omers-MacBook-Air-2.local>
2025-04-23 13:06:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner; // Note: Do not change this line.
+
+    public static void manageMovies() {}
+
+    public static void main(String[] args) throws IOException {
+        String path = args[0];
+
+        scanner = new Scanner(new File(path));
+        int numberOfTests = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 1; i <= numberOfTests; i++) {
+            System.out.println("Test number " + i + " starts.");
+            try {
+                manageMovies();
+            } catch(Exception e){
+                System.out.println("Exception " + e);
+            }
+            System.out.println("Test number " + i + " ended.");
+            System.out.println("-----------------------------------------------");
+        }
+        System.out.println("All tests have ended.");
+    }
+}
